574a616f164b293094f5f9f835ee79765c8746d1
Niseme <nijolesemele@gmail.com>
2022-03-21 15:46:06 +0200

    35.Arrow Function

--- script.js
+++ script.js
@@ -29,3 +29,42 @@ console.log(appleJuice);
 console.log(fruitProcessor(5, 0));
 
 console.log(fruitProcessor(7, 2));*/
+
+/*//34. Function Declaration vs. Expressions
+//function declaration
+function calcAge1(birthYear) {
+    return 2022 - birthYear;
+}
+
+const age1 = calcAge1(1989);
+console.log(age1);
+
+//function expression 
+const calcAge2 = function (birthYear) {
+    return 2022 - birthYear;
+}
+
+const age2 = calcAge2(1990);
+
+console.log(age1, age2);*/
+
+//35.Arrow Function
+
+//Function expression
+const calcAge2 = function (birthYear) {
+    return 2022 - birthYear;
+}
+
+//Arrow function
+const calcAge3 = birthYear => 2022 - birthYear;
+
+const age3 = calcAge3(1989);
+console.log(age3);
+
+const yearUntilRetirement = (birthYear, firstName) => {
+    const age = 2022 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retiret ar ${retirement}`;
+}
+
+console.log(yearUntilRetirement(1989, `Jonas`));
