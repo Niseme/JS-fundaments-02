5b5c30face3d17051500f11c02923c6a8ff22a46
Niseme <nijolesemele@gmail.com>
2022-03-21 15:12:07 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+/*//32. Activating Strict Mode
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log(`I can drive`);
+
+const interface = 'audio';*/
+
+/*//33. Functions
+'use strict';
+
+function logger() {
+    console.log(`My name is Jonas`);
+}
+
+//calling; running; invoking the function 
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+console.log(fruitProcessor(7, 2));*/
